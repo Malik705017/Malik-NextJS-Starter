2b1837d057bd8ae5232cf705ae784158556eceb2
Malik705017 <fhh1482@gmail.com>
2022-01-13 14:45:45 +0800

    feat: add useRedux hook

---
+++ hooks/useRedux.ts
@@ -0,0 +1,29 @@
+import { bindActionCreators, ActionCreatorsMapObject } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import type { RootState, AppDispatch } from '../models/store';
+
+export type Selector<ReturnState> = (s: RootState) => ReturnState;
+
+interface DefaultActionMap {
+  [key: string]: (...params: any[]) => void;
+}
+
+type WrapActionDispatch<ActionMap extends DefaultActionMap> = {
+  [key in keyof ActionMap]: (...params: Parameters<ActionMap[key]>) => void;
+};
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+const useAppDispatch = () => useDispatch<AppDispatch>();
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useRedux = <ReturnState, ActionMap extends DefaultActionMap>(
+  selector: Selector<ReturnState>,
+  actions: ActionCreatorsMapObject
+): [ReturnState, WrapActionDispatch<ActionMap>] => {
+  const state = useAppSelector(selector);
+  const dispatch = useAppDispatch();
+
+  const boundActions = bindActionCreators(actions, dispatch) as unknown;
+
+  return [state, boundActions as WrapActionDispatch<ActionMap>];
+};
