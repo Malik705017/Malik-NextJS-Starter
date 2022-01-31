051cf813bd37f81fc516f6a1b2cbbb4701839924
Malik705017 <fhh1482@gmail.com>
2022-01-31 21:40:10 +0800

    feat: add Carousel component

---
+++ components/common/organisms/Carousel/Carousel.tsx
@@ -0,0 +1,43 @@
+import { FC, useState, useEffect, ReactChild } from 'react';
+import classnames from 'classnames';
+
+import styles from './Carousel.module.scss';
+
+type CarouselProps = {
+  time?: number;
+  childNodes: ReactChild[];
+  className?: string;
+};
+
+const Carousel: FC<CarouselProps> = ({ time = 5000, childNodes, className = '' }) => {
+  const [activeSlideIndex, setActiveSlideIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setActiveSlideIndex((prevIndex) => {
+        if (prevIndex === childNodes.length - 1) {
+          return 0;
+        }
+        return prevIndex + 1;
+      });
+    }, time);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [time]);
+
+  return (
+    <div className={classnames(styles.carousel, className)}>
+      <ul>
+        {childNodes.map((node, index) => (
+          <li key={`_${index}`} className={classnames(styles.slide, activeSlideIndex === index && styles.activeSlide)}>
+            {node}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Carousel;
