c5db6369eb38e4b23402be2835bc4474a1d6f9cc
Malik705017 <fhh1482@gmail.com>
2022-02-04 20:54:36 +0800

    feat: revise Header component to show different UI due to auth state

--- components/common/molecules/Header/Header.tsx
+++ components/common/molecules/Header/Header.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react';
+import { useRouter } from 'next/router';
 
 import { useModal } from '../../../../models/modal';
+import { useAuth } from '../../../../models/auth';
+import { appRoute } from '../../../../utils/config/appRoute.config';
 
 import Icon from '../../atoms/Icon';
 
@@ -8,18 +11,30 @@ import styles from './Header.module.scss';
 
 const Header: FC = () => {
   const [, { openModal }] = useModal();
+  const [{ isLoggedIn }] = useAuth();
+  const router = useRouter();
 
   return (
     <div className={styles.header}>
-      <h1 className={styles.title}>Elsword 組團網</h1>
-      <Icon
-        className={styles.icon}
-        src="/icons/user-white.icon.png"
-        alt="userIcon"
-        width={36}
-        height={36}
-        onClick={() => openModal()}
-      />
+      <h1
+        className={styles.title}
+        onClick={() => {
+          router.push(appRoute.home);
+        }}
+      >
+        Elsword 組團網
+      </h1>
+      {isLoggedIn && <p>Welcome</p>}
+      {!isLoggedIn && (
+        <Icon
+          className={styles.icon}
+          src="/icons/user-white.icon.png"
+          alt="userIcon"
+          width={36}
+          height={36}
+          onClick={() => openModal()}
+        />
+      )}
     </div>
   );
 };
