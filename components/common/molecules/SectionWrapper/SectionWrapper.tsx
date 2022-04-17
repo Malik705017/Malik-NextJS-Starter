e04a0870482aba45a3405a11758a5ec3bad67208
Malik705017 <fhh1482@gmail.com>
2022-04-17 17:16:19 +0800

    fix: revise SectionWrapper component

--- components/common/molecules/SectionWrapper/SectionWrapper.tsx
+++ components/common/molecules/SectionWrapper/SectionWrapper.tsx
@@ -5,26 +5,14 @@ import styles from './SectionWrapper.module.scss';
 
 type SectionWrapperProps = {
   className?: string;
-  backgroundColor?: string;
   isFullPage?: boolean;
   children?: ReactNode;
 };
 
-const SectionWrapper: FC<SectionWrapperProps> = ({
-  children,
-  className = '',
-  backgroundColor = '',
-  isFullPage = false,
-}) => {
+const SectionWrapper: FC<SectionWrapperProps> = ({ children, className = '', isFullPage = false }) => {
   const wrapperClassName = classnames(className, styles.sectionWrapper, isFullPage && styles.fullpage);
 
-  return backgroundColor ? (
-    <section className={wrapperClassName} style={{ backgroundColor }}>
-      {children}
-    </section>
-  ) : (
-    <section className={wrapperClassName}>{children}</section>
-  );
+  return <section className={wrapperClassName}>{children}</section>;
 };
 
 export default SectionWrapper;
